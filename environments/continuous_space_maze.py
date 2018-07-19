[Critical miss fixed] Correct computation of sub-goal. Check matplotlib coordination in misc/test.py

environments/continuous_space_maze.py
@@ -26,10 +26,10 @@ class ContinuousSpaceMaze(Env, Serializable):
     def __init__(self, goal=(30, 40)):
         Serializable.quick_init(self, locals())
 
-        self.h1 = Hole(center=[23, 22], radius=14)
+        # self.h1 = Hole(center=[23, 22], radius=14)
 
         # single path
-        # self.h1 = Hole(center=[26, 15], radius=21)
+        self.h1 = Hole(center=[32, 20], radius=21)
 
         self.h2 = Hole(center=[8, 42], radius=8)
         # self.goal = np.array([30, 40])
@@ -108,7 +108,7 @@ def think_maze_layout():
     # h1 = Hole(center=[25, 20], radius=14)
     # h2 = Hole(center=[10, 40], radius=8)
     plt.style.use('mystyle3')
-    h1 = Hole(center=[26, 15], radius=21)
+    h1 = Hole(center=[32, 20], radius=21)
     h2 = Hole(center=[8, 42], radius=8)
 
     print(np.linalg.norm(h1.c - h2.c) - h1.r - h2.r)
@@ -124,13 +124,13 @@ def think_maze_layout():
     #     for j in range(a.shape[1]):
     #         text = ax.text(j, i, s[i][j],
     #                         ha="center", va="center", color="w")
-
-    c1 = patches.Circle(xy=h1.c, radius=h1.r, fc='k', ec='k')
-    c2 = patches.Circle(xy=h2.c, radius=h2.r, fc='k', ec='k')
+    offset = np.array([0.5, 0.5])
+    c1 = patches.Circle(xy=h1.c - offset, radius=h1.r, fc='k', ec='k')
+    c2 = patches.Circle(xy=h2.c - offset, radius=h2.r, fc='k', ec='k')
     ax.add_patch(c1)
     ax.add_patch(c2)
     goal = [20, 45]
-    ax.text(goal[0], goal[1], 'G', horizontalalignment='center',
+    ax.text(goal[0] - offset[0], goal[1] - offset[1], 'G', horizontalalignment='center',
                          verticalalignment='center', fontsize=8)
 
     plt.show()

misc/plotter.py
@@ -77,55 +77,6 @@ def plot_log(log_file, save_path=None):
 
     plt.show()
 
-def plot_map(map_files, is_mask=False, save_path=None):
-    env = ContinuousSpaceMaze()
-    plt.style.use('mystyle3')
-    fig, axes = plt.subplots(2, 2)
-    plt.tick_params(labelbottom=False, labelleft=False, labelright=False, labeltop=False)
-
-    # plot maze
-    a = np.zeros([50, 50])
-    im = axes[0, 0].imshow(a, cmap='binary')
-    c1 = patches.Circle(xy=env.h1.c, radius=env.h1.r, fc='k', ec='k')
-    c2 = patches.Circle(xy=env.h2.c, radius=env.h2.r, fc='k', ec='k')
-    axes[0, 0].add_patch(c1)
-    axes[0, 0].add_patch(c2)
-
-    # data load
-    data = np.load(map_files[-1])
-    v_map = data['v_map'].reshape(25, 25)
-    knack_map = data['knack_map'].reshape(25, 25)
-    knack_map_kurtosis = data['knack_map_kurtosis'].reshape(25, 25)
-
-    v_map = map_reshaper(v_map)
-    knack_map = map_reshaper(knack_map)
-    knack_map_kurtosis = map_reshaper(knack_map_kurtosis)
-    if is_mask:
-        mask = np.ones([50, 50])
-        for i in range(50):
-            for j in range(50):
-                if (np.linalg.norm(np.asarray([j, i]) - env.h1.c) <= env.h1.r) or (np.linalg.norm(np.asarray([i, j]) - env.h2.c) <= env.h2.r):
-                    mask[j, i] = 0.
-
-        v_map[mask == 0] = np.min(v_map)
-        knack_map[mask == 0] = np.min(knack_map)
-        knack_map_kurtosis[mask == 0] = np.min(knack_map_kurtosis)
-        # we have already set color-bar-min as 0.
-        # v_map[mask == 0] = 0.
-        # knack_map[mask == 0] = 0.
-        # knack_map_kurtosis[mask == 0] = 0.
-
-    axes[0, 1].imshow(v_map, cmap='Reds')
-    axes[0, 1].set_title('V(s)')
-    axes[1, 0].imshow(knack_map, cmap='Reds')
-    axes[1, 0].set_title('knack map')
-    axes[1, 1].imshow(knack_map_kurtosis, cmap='Reds')
-    axes[1, 1].set_title('knack map kurtosis')
-    c1 = patches.Circle(xy=env.h1.c, radius=env.h1.r, fc='k', ec='k', alpha=0.1)
-    c2 = patches.Circle(xy=env.h2.c, radius=env.h2.r, fc='k', ec='k', alpha=0.1)
-    axes[1, 1].add_patch(c1)
-    axes[1, 1].add_patch(c2)
-    plt.show()
 
 def map_reshaper(map):
     """reshape 25x25 to 50x50"""
@@ -136,7 +87,7 @@ class map_animation_maker(object):
     """
     see misc/test.py for simpler program
     """
-    def __init__(self, root_dir):
+    def __init__(self, root_dir, is_mask=True):
         self.map_files = glob(os.path.join(root_dir, 'maps/*.npz'))
 
         # figure configuration
@@ -163,9 +114,11 @@ class map_animation_maker(object):
         self.im11 = self.axes[1, 1].imshow(tmp, cmap='Reds', animated=True, vmin=0., vmax=1.)
 
         # prepare to draw hole
-        self.env = ContinuousSpaceMaze()
-        hole1 = patches.Circle(xy=self.env.h1.c, radius=self.env.h1.r, fc='k', ec='k')
-        hole2 = patches.Circle(xy=self.env.h2.c, radius=self.env.h2.r, fc='k', ec='k')
+        self.env = ContinuousSpaceMaze(goal=(20, 45))
+        # off set to draw on imshow coordinate (see misc.test.plot_test)
+        offset = np.array([0.5, 0.5])
+        hole1 = patches.Circle(xy=self.env.h1.c - offset, radius=self.env.h1.r, fc='k', ec='k')
+        hole2 = patches.Circle(xy=self.env.h2.c - offset, radius=self.env.h2.r, fc='k', ec='k')
         self.axes[0, 0].add_patch(hole1)
         self.axes[0, 0].add_patch(hole2)
         self.axes[0, 0].text(0.5, 0.5, 'S', horizontalalignment='center', verticalalignment='center', fontsize=5)
@@ -174,20 +127,23 @@ class map_animation_maker(object):
         #                      verticalalignment='center', fontsize=5)
 
         # to avoid re-use artist, re-define
-        hole1 = patches.Circle(xy=self.env.h1.c, radius=self.env.h1.r, fc='k', ec='k', alpha=0.2)
-        hole2 = patches.Circle(xy=self.env.h2.c, radius=self.env.h2.r, fc='k', ec='k', alpha=0.2)
+        hole1 = patches.Circle(xy=self.env.h1.c - offset, radius=self.env.h1.r, fc='k', ec='k', alpha=0.2)
+        hole2 = patches.Circle(xy=self.env.h2.c - offset, radius=self.env.h2.r, fc='k', ec='k', alpha=0.2)
         self.circles = [self.axes[0, 1].add_patch(hole1), self.axes[0, 1].add_patch(hole2)]
-        hole1 = patches.Circle(xy=self.env.h1.c, radius=self.env.h1.r, fc='k', ec='k', alpha=0.2)
-        hole2 = patches.Circle(xy=self.env.h2.c, radius=self.env.h2.r, fc='k', ec='k', alpha=0.2)
+        hole1 = patches.Circle(xy=self.env.h1.c - offset, radius=self.env.h1.r, fc='k', ec='k', alpha=0.2)
+        hole2 = patches.Circle(xy=self.env.h2.c - offset, radius=self.env.h2.r, fc='k', ec='k', alpha=0.2)
         self.circles.extend([self.axes[1, 0].add_patch(hole1), self.axes[1, 0].add_patch(hole2)])
-        hole1 = patches.Circle(xy=self.env.h1.c, radius=self.env.h1.r, fc='k', ec='k', alpha=0.2)
-        hole2 = patches.Circle(xy=self.env.h2.c, radius=self.env.h2.r, fc='k', ec='k', alpha=0.2)
+        hole1 = patches.Circle(xy=self.env.h1.c - offset, radius=self.env.h1.r, fc='k', ec='k', alpha=0.2)
+        hole2 = patches.Circle(xy=self.env.h2.c - offset, radius=self.env.h2.r, fc='k', ec='k', alpha=0.2)
         self.circles.extend([self.axes[1, 1].add_patch(hole1), self.axes[1, 1].add_patch(hole2)])
+
+        # whether to mask values of state in hole
+        self.is_mask=is_mask
 
     def updateifig(self, i):
         # print(i)
         title = self.fig.suptitle("epoch{}".format(i), fontsize=5)
-        data = self.load_map_data(self.map_files[i*2], is_mask=True)
+        data = self.load_map_data(self.map_files[i*2], is_mask=self.is_mask)
         self.im01.set_array(data['v_map'])
         self.im10.set_array(data['knack_map'])
         self.im11.set_array(data['knack_map_kurtosis'])
@@ -222,8 +178,9 @@ class map_animation_maker(object):
             mask = np.ones([50, 50])
             for i in range(50):
                 for j in range(50):
-                    if (np.linalg.norm(np.asarray([j, i]) - self.env.h1.c) <= self.env.h1.r) or (np.linalg.norm(np.asarray([i, j]) - self.env.h2.c) <= self.env.h2.r):
-                        mask[j, i] = 0.
+                    # arr[i, j] means (x, y) = (j, i)
+                    if (np.linalg.norm(np.asarray([j, i]) - self.env.h1.c) <= self.env.h1.r) or (np.linalg.norm(np.asarray([j, i]) - self.env.h2.c) <= self.env.h2.r):
+                        mask[i, j] = 0.
 
             # v_map[mask == 0] = np.min(v_map)
             # knack_map[mask == 0] = np.min(knack_map)
@@ -243,7 +200,7 @@ def normalize(arr):
     arr = arr / M
     return arr
 
-def continuous_maze_plot(root_dir):
+def continuous_maze_plot(root_dir, is_mask=True):
     save_path = os.path.join(root_dir, 'graphs')
     os.makedirs(save_path, exist_ok=True)
     log_file = os.path.join(root_dir, 'log.json')
@@ -253,7 +210,7 @@ def continuous_maze_plot(root_dir):
     # plot_map(map_files=map_files, is_mask=False)
     # plot_map(map_files=map_files, is_mask=True)
 
-    ani = map_animation_maker(root_dir=root_dir)
+    ani = map_animation_maker(root_dir=root_dir, is_mask=is_mask)
     ani.animate(save_path=save_path)
 
 
@@ -266,4 +223,4 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
-    continuous_maze_plot(args['root_dir'])
+    continuous_maze_plot(args['root_dir'], is_mask=False)

misc/test.py
@@ -28,6 +28,7 @@ def animation_test():
             self.map_files = glob('/home/karino/mount/NormalizedContinuousSpaceMaze-0718-01h-26m-1531844792s/maps/*.npz')
             self.env = ContinuousSpaceMaze()
             d = self.load_map_data(map_file=self.map_files[-1])
+            # !!!!!!!!! vmin and vmax are very important. it is not changeable
             self.im2 = self.axes[0, 1].imshow(d['v_map'], animated=True, vmin=0., vmax=1.)
 
         def updateifig(self, i, *args):
@@ -105,5 +106,39 @@ def animation_test():
     a = Anim()
     a.animate()
 
+
+def plot_test():
+    """
+    Test the order of matplot imshow
+    Test the coordinate
+
+    order:
+    arr[0][0], arr[0][1], ...
+    arr[1][0], arr[1][0], ...
+    ...
+
+    !!! The coordinate have -0.5 offset !!!
+    The coordinate origin is upper left
+    if we use relatively adjust coordinate, - [0.5, 0.5]
+
+    """
+    fig, ax = plt.subplots(1)
+    im = np.arange(25).reshape(5, 5)
+    print(im)
+
+    offset = np.array([0.5, 0.5])
+    center = np.array([2, 3])  # [x, y]
+    c = patches.Circle(xy=center-offset, radius=2)
+    ax.add_patch(c)
+
+    # white means small, black does large value
+    ax.imshow(im, cmap='binary')
+
+    plt.show()
+
+
+
+
 if __name__ == '__main__':
-    animation_test()
+    # animation_test()
+    plot_test()

sac/algos/base.py
@@ -113,15 +113,6 @@ class RLAlgorithm(Algorithm):
                     #     np.savez(save_path, v_map=v_map, knack_map=knack_map, knack_map_kurtosis=knack_map_kurtosis)
                     #     save_knack_episodes += 50
 
-                mylogger.write()
-                v_map, knack_map, knack_map_kurtosis = self._value_and_knack_map()
-                save_path = os.path.join(mylogger._my_map_log_dir, 'epoch' + str(epoch) + '.npz')
-                np.savez(save_path, v_map=v_map, knack_map=knack_map, knack_map_kurtosis=knack_map_kurtosis)
-                if epoch % 10 == 0:
-                    saver.save(self._sess, os.path.join(mylogger._my_log_parent_dir, 'model'))
-                if dynamic_ec:
-                    self._sess.run(tf.assign(_ec, _ec - dicrese_rate))
-
                 self._evaluate(epoch)
 
                 params = self.get_snapshot(epoch)
@@ -142,6 +133,15 @@ class RLAlgorithm(Algorithm):
                     logger.record_tabular('obs_var', env._obs_var)
 
                 self.sampler.log_diagnostics()
+
+                mylogger.write()
+                v_map, knack_map, knack_map_kurtosis = self._value_and_knack_map()
+                save_path = os.path.join(mylogger._my_map_log_dir, 'epoch' + str(epoch) + '.npz')
+                np.savez(save_path, v_map=v_map, knack_map=knack_map, knack_map_kurtosis=knack_map_kurtosis)
+                if epoch % 10 == 0:
+                    saver.save(self._sess, os.path.join(mylogger._my_log_parent_dir, 'model'))
+                if dynamic_ec:
+                    self._sess.run(tf.assign(_ec, _ec - dicrese_rate))
 
                 logger.dump_tabular(with_prefix=False)
                 logger.pop_prefix()
@@ -176,6 +176,10 @@ class RLAlgorithm(Algorithm):
         logger.record_tabular('episode-length-min', np.min(episode_lengths))
         logger.record_tabular('episode-length-max', np.max(episode_lengths))
         logger.record_tabular('episode-length-std', np.std(episode_lengths))
+
+        mylogger.data_update(key='eval_average_return', val=np.mean(total_returns))
+        terminal_states = [path['next_observations'][-1].tolist() for path in paths]
+        mylogger.data_update(key='eval_terminal_states', val=terminal_states)
 
         self._eval_env.log_diagnostics(paths)
         if self._eval_render:

sac/algos/sac.py
@@ -456,31 +456,32 @@ class SAC(RLAlgorithm, Serializable):
         self.q_for_state_importance_ops = self._qf.get_output_for(self._observations_ph, actions, reuse=True)
         self.test_states = np.array([[i, j] for j in range(0, 50, 2) for i in range(0, 50, 2)])
         tests = self.test_states
-        self.test_N = 100
+        self.test_N = 1000
         for i in range(self.test_N-1):
             tests = np.concatenate((tests, self.test_states))
         self.tests_q = tests
 
     @overrides
     def _value_and_knack_map(self):
+        """Debug done: Correct computation"""
         test_states = self.test_states
         tests_q = self.tests_q
 
         if hasattr(self.sampler, 'obs_mean'):
-            test_states = self.sampler.apply_normalize_obs(test_states)
+            test_states = self.sampler.apply_normalize_obs(test_states)  # debug OK
             b = test_states
             for i in range(self.test_N - 1):
-                b = np.concatenate((b, self.test_states))
-            tests_q = b
+                b = np.concatenate((b, test_states))
+            tests_q = b  # debug OK: correctly copy states
 
         v_map = self._sess.run(self._vf_t, feed_dict={self._observations_ph: test_states})
-        q_values = self._sess.run(self.q_for_state_importance_ops, feed_dict={self._observations_ph: tests_q})
-        q_values = q_values.reshape(self.test_N, len(self.test_states))
+        q_values = self._sess.run(self.q_for_state_importance_ops, feed_dict={self._observations_ph: tests_q})  # debug OK: correctly sample different action per same state
+        q_values = q_values.reshape(self.test_N, len(test_states))  # debug OK: Correct order by reshape
         q_1_moment = np.mean(q_values, axis=0)
-        q_2 = np.square(q_values)
-        q_2_moment = np.mean(q_2, axis=0)
-        q_4_moment = np.mean(np.square(q_2), axis=0)
-        knack_map = q_2_moment - q_1_moment
+        diffs_pow2 = np.square(q_values - q_1_moment)
+        q_2_moment = np.mean(diffs_pow2, axis=0)
+        q_4_moment = np.mean(np.square(diffs_pow2), axis=0)
+        knack_map = q_2_moment
         knack_map_kurtosis = q_4_moment / np.square(q_2_moment)
 
         return v_map, knack_map, knack_map_kurtosis